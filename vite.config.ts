f312c95ac7ed8f36f17cd89e3fd7b0293f3f880a
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2025-09-26 22:42:23 +0500

    feat: environment validator setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, type ServerOptions } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()]
+// export default defineConfig({
+//     plugins: [react(), tailwindcss()]
+// })
+
+type TMode = 'development' | 'production'
+
+interface AppEnv {
+    VITE_ENV: TMode
+    PORT: number
+}
+
+const validateEnv = (mode: TMode, env: AppEnv) => {
+    const requiredVars: (keyof AppEnv)[] = ['VITE_ENV', 'PORT']
+
+    for (const variable of requiredVars) {
+        if (!(variable in env)) {
+            throw new Error(`Missing required environment variable: ${variable} in .env.${mode}`)
+        }
+    }
+}
+
+export default defineConfig(({ mode }) => {
+    const modeType = mode as TMode
+
+    const env = loadEnv(modeType, process.cwd(), '') as unknown as AppEnv
+
+    validateEnv(modeType, env)
+
+    const config: ServerOptions = {
+        port: env.PORT,
+        open: true
+    }
+    return {
+        plugins: [react(), tailwindcss()],
+        server: config,
+        preview: config,
+        build: {
+            minify: true
+        }
+    }
 })
